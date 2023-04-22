190899a5e2fda267dace29b853dc1f59cc4a4cd3
bella <bwei60@gatech.edu>
2023-04-22 10:54:09 -0700

    created movie model

---
+++ backend/src/test/java/dev/bella/movies/Movie.java
@@ -0,0 +1,89 @@
+package dev.bella.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLinker;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getImdbId() {
+        return imdbId;
+    }
+
+    public void setImdbId(String imdbId) {
+        this.imdbId = imdbId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getTrailerLinker() {
+        return trailerLinker;
+    }
+
+    public void setTrailerLinker(String trailerLinker) {
+        this.trailerLinker = trailerLinker;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<String> genres) {
+        this.genres = genres;
+    }
+
+    public List<String> getBackdrops() {
+        return backdrops;
+    }
+
+    public void setBackdrops(List<String> backdrops) {
+        this.backdrops = backdrops;
+    }
+}
