73262b04654e8a929337b0b5ca0fb4774e231451
bella <bwei60@gatech.edu>
2023-04-22 17:09:48 -0700

    update movie model and movie app

--- backend/src/main/java/dev/bella/movies/MoviesApplication.java
+++ backend/src/main/java/dev/bella/movies/MoviesApplication.java
@@ -2,6 +2,7 @@ package dev.bella.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MoviesApplication {
@@ -9,4 +10,5 @@ public class MoviesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+
 }
